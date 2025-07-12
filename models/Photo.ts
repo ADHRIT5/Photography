ca99f455f9634cc4f60356870598124719b90af5
Abhay Kumar Yadav <indianlancer1802@gmail.com>
2025-07-12 17:24:52 +0530

    Update Photo.ts

--- models/Photo.ts
+++ models/Photo.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const CommentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
+const commentSchema = new mongoose.Schema({
+  text: String,
   createdAt: { type: Date, default: Date.now },
-})
+});
 
-const PhotoSchema = new mongoose.Schema({
+const photoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: String,
   imageUrl: { type: String, required: true },
   likes: { type: Number, default: 0 },
-  comments: [CommentSchema],
+  comments: [commentSchema],
   createdAt: { type: Date, default: Date.now },
-})
+});
 
-export default mongoose.models.Photo || mongoose.model("Photo", PhotoSchema)
+export default mongoose.models.Photo || mongoose.model("Photo", photoSchema);
